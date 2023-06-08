67408aa4de13a0588a41a2385527fc568cf54d56
Guilherme Costa <costaguilherme1995@gmail.com>
2023-06-08 16:16:21 +0200

    refactoring vans.jsx page

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { Link, Outlet, useSearchParams } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom';
+import { getVans } from '/api.js'
 
 function Vans() {
     const [searchParams, setSearchParams] = useSearchParams();
     const [vans, setVans] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null)
 
     const typeFilter = searchParams.get('type');
 
-
     useEffect(() => {
-        fetch('/api/vans')
-            .then(res => res.json())
-            .then(data => {
-                setVans(data.vans)
-            })
-            .catch((error) => {
-                console.log("error", error);
-            });
+        async function loadVans() {
+            setLoading(true)
+            try {
+                const data = await getVans()
+                setVans(data)
+
+            } catch (err) {
+                setError(err)
+            } finally {
+                setLoading(false)
+            }
+        }
+        loadVans()
     }, []);
 
     const displayedVans =
@@ -35,64 +42,72 @@ function Vans() {
         })
     }
 
-    return (
-        <div className='vans--container'>
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+
+    if (error) {
+        return <h1>There was an error : {error.message}</h1>
+    }
+
+        return (
+            <div className='vans--container'>
 
 
-            <h1 className='vans--title'>Explore our van options</h1>
-            <div className='vans--filter'>
-                <button
-                    onClick={() => handleFilterChange('type', 'simple')}
-                    className={`simple--btn ${typeFilter === 'simple' ? 'selected' : ''}`}
-                >
-                    Simple
-                    {console.log(typeFilter)}
-                </button>
-
-                <button
-                    onClick={() => handleFilterChange('type', 'luxury')}
-                    className={`luxury--btn ${typeFilter === 'luxury' ? 'selected' : ''}`}                >
-                    Luxury
-                </button>
-                <button
-                    onClick={() => handleFilterChange('type', 'rugged')}
-                    className={`rugged--btn ${typeFilter === 'rugged' ? 'selected' : ''}`}                >
-                    Rugged
-                </button>
-
-                {typeFilter ? (
+                <h1 className='vans--title'>Explore our van options</h1>
+                <div className='vans--filter'>
                     <button
-                        onClick={() => handleFilterChange('type', null)}
-                        className='clear--btn'
+                        onClick={() => handleFilterChange('type', 'simple')}
+                        className={`simple--btn ${typeFilter === 'simple' ? 'selected' : ''}`}
                     >
-                        Clear filters
+                        Simple
+                        {console.log(typeFilter)}
                     </button>
-                ) : null}
+
+                    <button
+                        onClick={() => handleFilterChange('type', 'luxury')}
+                        className={`luxury--btn ${typeFilter === 'luxury' ? 'selected' : ''}`}                >
+                        Luxury
+                    </button>
+                    <button
+                        onClick={() => handleFilterChange('type', 'rugged')}
+                        className={`rugged--btn ${typeFilter === 'rugged' ? 'selected' : ''}`}                >
+                        Rugged
+                    </button>
+
+                    {typeFilter ? (
+                        <button
+                            onClick={() => handleFilterChange('type', null)}
+                            className='clear--btn'
+                        >
+                            Clear filters
+                        </button>
+                    ) : null}
+                </div>
+
+
+                <div className='vans--list'>
+                    {displayedVans.map((van, vanIndex) => (
+                        <div key={vanIndex} className='vans--item'>
+                            <Link
+                                to={van.id}
+                                state={{
+                                    search: `?${searchParams.toString()}`,
+                                    type: typeFilter
+                                }}>
+                                <img className='van--img' src={van.imageUrl} alt='van image' />
+                                <h3 className='van--name'>{van.name}</h3>
+                                <h3 className='van--price'>€{van.price}<span className='day'>/day</span></h3>
+                                <button className=
+                                    {`van--btn ${van.type === 'simple' ? 'simple--btn' :
+                                        van.type === 'luxury' ? 'luxury--btn' : van.type === 'rugged' ? 'rugged--btn' : ''}`}>{van.type}</button>
+                            </Link>
+                        </div>
+
+                    ))}
+                </div>
             </div>
-
-
-            <div className='vans--list'>
-                {displayedVans.map((van, vanIndex) => (
-                    <div key={vanIndex} className='vans--item'>
-                        <Link
-                            to={van.id}
-                            state={{
-                                search: `?${searchParams.toString()}`,
-                                type: typeFilter
-                            }}>
-                            <img className='van--img' src={van.imageUrl} alt='van image' />
-                            <h3 className='van--name'>{van.name}</h3>
-                            <h3 className='van--price'>€{van.price}<span className='day'>/day</span></h3>
-                            <button className=
-                                {`van--btn ${van.type === 'simple' ? 'simple--btn' :
-                                    van.type === 'luxury' ? 'luxury--btn' : van.type === 'rugged' ? 'rugged--btn' : ''}`}>{van.type}</button>
-                        </Link>
-                    </div>
-
-                ))}
-            </div>
-        </div>
-    )
+        )
 }
 
 export default Vans
