8718e1cce93c68a1f8c4e3f9ec4d5dd5c0b09b03
Guilherme Costa <costaguilherme1995@gmail.com>
2023-06-09 16:15:55 +0200

    Implemented the useRouteError

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import { useLoaderData } from 'react-router-dom';
+import React from 'react';
+import { useRouteError } from 'react-router-dom';
+
 
 function Error() {
-    const data = useLoaderData
-        return (
-            <div>
-                <h2>An error occurred</h2>
-            </div>
-        )
-    }
+    const error = useRouteError()
+    console.log(error)
+    return (
+        <div>
+            <h2>Error:{error.message}</h2>
+            <pre>{error.status} - {error.statusText}</pre>
+        </div>
+    )
+}
 
 export default Error
